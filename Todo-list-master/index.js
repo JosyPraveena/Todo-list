7f07bd57ac9f391e3caebd1de959aa057f0ea4d5
JosyPraveena <josypraveena@gmail.com>
2020-04-10 09:51:29 +0200

    added delete icon and Character check

--- Todo-list-master/index.js
+++ Todo-list-master/index.js
@@ -4,7 +4,7 @@ const click = document.querySelector(".add-todo-section");
 console.log(click);
 
 function addItem(text) {
-  const template = `<input class="item-checkbox" type="checkbox"/><li>${text}</li><button class="edit-button"><i class="fas  fa-pencil-alt fa-2x"></i></button>`;
+  const template = `<input class="item-checkbox" type="checkbox"/><li>${text}</li><button class="edit-button"><i class="fas  fa-pencil-alt fa-2x"></i></button><button class="delete-button"><i class="fas  fa-trash-alt fa-2x"></i></button>`;
   document.querySelector(".todo").innerHTML += template;
 }
 
@@ -18,3 +18,17 @@ click.addEventListener("submit", (event) => {
   }
   event.preventDefault();
 });
+
+
+const characterCheck = document.querySelector("#character-check p");
+console.log(characterCheck);
+let limit = 100;
+
+text.addEventListener("keyup", (event) => {
+  characterCheck.innerHTML = limit - text.value.length + " characters more";
+  if (text.value.length >= limit) {
+    text.value = text.value.substring(0, limit);
+    characterCheck.innerHTML = "0 Letters Remaining";
+  }
+});
+
